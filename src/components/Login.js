dd71b3282554712d040037b1d03e1585979323ff
Charitha2009 <70010999+Charitha2009@users.noreply.github.com>
2024-05-21 23:12:48 +0530

    created design for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,28 @@
 // Login.js
 import React from 'react';
+import '../Styles/Login.css';
+import Navbar from './Navbar';
 
 const Login = () => {
   return (
     <div>
-      <h1>Login</h1>
-      <form>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" />
+      <Navbar />
+      <div className='login-container'>
+        <div className='login-form-container'>
+          <h1>Login</h1>
+          <form>
+            <div>
+              {/* <label htmlFor="username">Username:</label> */}
+              <input type="text" id="username" name="username" placeholder="Username" />
+            </div>
+            <div>
+              {/* <label htmlFor="password">Password:</label> */}
+              <input type="password" id="password" name="password" placeholder="Password" />
+            </div>
+            <button type="submit">Login</button>
+          </form>
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      </div>
     </div>
   );
 };
